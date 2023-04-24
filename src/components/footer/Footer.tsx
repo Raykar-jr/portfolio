03ecaac366bd39cf0d97d5bcf80c02d1d873ac28
Raykar-jr <kirillazarenk@mail.ru>
2023-04-24 18:33:41 +0300

    feat: font awesome icons update

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import s from './Footer.module.scss'
-
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import { faGithub, faTelegram, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 export const Footer = () => {
 
     return (
@@ -9,14 +10,15 @@ export const Footer = () => {
                      <h2 className={s.title}>Kiryl
                          <span> Azaranka</span></h2>
                      <div className={s.icons}>
+
                          <a href="https://github.com/Raykar-jr">
-                             <span style={{color: 'gray'}} className="fa-brands fa-github fa-2xl"></span>
+                             <FontAwesomeIcon icon={faGithub} size='2xl' style={{color: 'gray'}} />
                          </a>
                          <a href="https://t.me/raykar_jr">
-                             <span style={{color: 'gray'}} className="fa-brands fa-telegram fa-2xl"></span>
+                             <FontAwesomeIcon icon={faTelegram} size='2xl' style={{color: 'gray'}} />
                          </a>
                          <a href="https://www.linkedin.com/in/kiryl-azaranka-a45b6b25a/">
-                             <span style={{color: 'gray'}} className="fa-brands fa-linkedin fa-2xl"></span>
+                             <FontAwesomeIcon icon={faLinkedin} size='2xl' style={{color: 'gray'}} />
                          </a>
 
                      </div>
